27d8cb78f94f3c4112d9af2553c910311c747b7c
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-07-22 21:56:23 +0530

    java updatd

---
+++ FileHandeling/Create.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+public class Create {
+    public static void main(String[] args) {
+        try {
+            File myFile=new File("")
+        } catch (Exception e) {
+           
+        }
+    }
+}
---
+++ FileHandeling/Files.java
@@ -0,0 +1,7 @@
+import java.io.File;
+public class Files {
+    public static void main(String[] args) {
+        File myFile = new File("hello.txt");
+        System.out.println(myFile);
+    }
+}
