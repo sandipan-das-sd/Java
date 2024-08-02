d8f498392e010fae769059ad8420ac406ae9fd78
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-08-02 08:00:56 +0530

    opps

--- Oops/Opps.java
+++ Oops/Opps.java
@@ -4,6 +4,12 @@ public class Opps {
         pen1.color = "blue";
         pen1.type = "gel";
         pen1.write();
+        Pen pen2 = new Pen();
+        pen2.color = "green";
+        pen2.type = "ge2";
+        pen2.write();
+        pen2.printColor();
+        pen1.printColor();
     }
 }
 
@@ -14,4 +20,27 @@ class Pen {
     public void write() {
         System.out.println("Writing something...");
     }
+
+    public void printColor() {
+        System.out.println(this.color);
+    }
+
 }
+
+class Student {
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+
+
+
+
+
+
+
+
